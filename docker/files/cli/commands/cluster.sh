f22bc4a0c8f4798d050879e243b1a1a4a5b5aaac
j.m.dejager <j.m.dejager@syslogic.nl>
2019-02-01 11:17:28 +0100

    better error handling and comment based hostgroup rules

--- docker/files/cli/commands/cluster.sh
+++ docker/files/cli/commands/cluster.sh
@@ -30,14 +30,29 @@ command_init() {
     touch /proxysql-ready
 }
 
-commands_add "cluster:query [QUERY]" "perform mysql query on the cluster"
+commands_add "query [QUERY]" "perform mysql query on the cluster"
 command_query() {
-    echo $(proxysql_execute_query "$1")
+    proxysql_execute_query_hr "$1"
+}
+
+commands_add "query:users" "show mysql_users"
+command_query:users() {
+    proxysql_execute_query_hr "SELECT username, password,  FROM mysql_users"
+}
+
+commands_add "query:servers" "show mysql_servers"
+command_query:servers() {
+    proxysql_execute_query_hr "SELECT * FROM mysql_servers"
+}
+
+commands_add "query:rules" "show mysql_servers"
+command_query:rules() {
+    proxysql_execute_query_hr "SELECT * FROM mysql_query_rules"
 }
 
 commands_add "show:nodes" "Show al the nodes of the cluster"
 command_query:nodes() {
-    proxysql_execute_query "SELECT * FROM proxysql_servers;"
+    proxysql_execute_query_hr "SELECT * FROM proxysql_servers;"
 }
 
 commands_add "sync" "Synchronize from backends"
@@ -67,6 +82,7 @@ command_sync() {
         availableDatabases=$(mysql_execute_query "
             SELECT QUOTE(SCHEMA_NAME)
             FROM INFORMATION_SCHEMA.SCHEMATA
+            WHERE SCHEMA_NAME NOT IN ('information_schema', 'performance_schema', 'sys')
         " ${hostname});
 
         databaseCount=$(wc -l <<< "${availableDatabases}")
@@ -79,6 +95,10 @@ command_sync() {
         databasesString=$(echo "${availableDatabases}" | awk -vORS=, '{ print $1 }' | sed 's/,$/\n/')
 
         echo -e "\e[33m Adding schema rules... \n --- \e[0m"
+
+        proxysql_execute_query  "
+            INSERT INTO mysql_query_rules (active, match_pattern, destination_hostgroup, apply )
+            VALUES (1, '\/\*.*\s*hg\s*=\s*${hostgroup}\s*.*\*\/', '${hostgroup}', 1);"
 
         while read database; do
 
@@ -99,7 +119,11 @@ command_sync() {
 
             proxysql_execute_query "
                 INSERT INTO mysql_users (username, password, default_schema)
-                VALUES ('${username}', '${password}', '${database}');"
+                VALUES ('${username}', '${password}', '${database}');" &> /dev/null
+
+            if [[ ${?} -eq 1 ]]; then
+                echo -e "Adding ${username}:${database} failed"
+            fi
 
         done
 
@@ -108,8 +132,8 @@ command_sync() {
     echo -e "\e[33m Setting default route group: ${newDefaultHostgroup} (${newDefaultHostgroupCount} database) \e[0m"
 
     proxysql_execute_query  "
-        UPDATE mysql_users
-        SET default_hostgroup = '${newDefaultHostgroup}'"
+        UPDATE mysql_users SET default_hostgroup=${newDefaultHostgroup}, transaction_persistent=0
+        WHERE username = '${MYSQL_ADMIN_USERNAME}';"
 
     proxysql_execute_query "
         LOAD MYSQL VARIABLES TO RUN;
