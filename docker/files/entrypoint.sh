943bfed323332f7cab3b1960c7c83c0766b31dd5
R.J.J. van der Kolk | Scienta <r.j.j.vanderkolk@syslogic.local>
2019-07-16 15:25:12 +0200

    Init with check

--- docker/files/entrypoint.sh
+++ docker/files/entrypoint.sh
@@ -9,33 +9,32 @@ fi
 if [[ "${1}" = "--cluster" ]]; then
     proxysql-cli init &
 	set -- proxysql -f --initial
-
-	# test
-	# count number of available mysql_servers
-	count=$(mysql -u${PROXYSQL_ADMIN_USERNAME} -p${PROXYSQL_ADMIN_PASSWORD} -h127.0.0.1 -P6032 -se "SELECT COUNT (*) FROM mysql_servers")
-  echo $count
-	# count number of offline mysql_servers
-  mysql -u${PROXYSQL_ADMIN_USERNAME} -p${PROXYSQL_ADMIN_PASSWORD} -h127.0.0.1 -P6032 -se "SELECT hostgroup_id,status FROM mysql_servers" | while read hostgroup_id status; do
-    # use $hostgroup_id and $status variables
-    echo "hostID: $hostgroup_id, status: $status"
-    if [ ! "$status" = "ONLINE" ]; then
-      var=$((var + 1))
-      echo "It's offline..." $var
-      echo "Found:" $var " of " $count " servers total"
-        if [ $var = $count ]; then
-				 # all server are offline
-         echo "Kill it with FIRE!" 
-        fi
-    else
-      echo "It's online... Proceed init"
-    fi
-  done
-
 elif [[ "${1}" = "--sync_cluster" ]]; then
 	proxysql -f --initial &> /dev/null &
 	set -- proxysql-cli sync:cluster
 else
 	set -- proxysql -f "$@"
 fi
+
+# test
+# count number of available mysql_servers
+count=$(mysql -u${PROXYSQL_ADMIN_USERNAME} -p${PROXYSQL_ADMIN_PASSWORD} -h127.0.0.1 -P6032 -se "SELECT COUNT (*) FROM mysql_servers")
+echo $count
+# count number of offline mysql_servers
+mysql -u${PROXYSQL_ADMIN_USERNAME} -p${PROXYSQL_ADMIN_PASSWORD} -h127.0.0.1 -P6032 -se "SELECT hostgroup_id,status FROM mysql_servers" | while read hostgroup_id status; do
+	# use $hostgroup_id and $status variables
+	echo "hostID: $hostgroup_id, status: $status"
+	if [ ! "$status" = "ONLINE" ]; then
+		var=$((var + 1))
+		echo "It's offline..." $var
+		echo "Found:" $var " of " $count " servers total"
+			if [ $var = $count ]; then
+			 # all server are offline
+			 echo "Kill it with FIRE!"
+			fi
+	else
+		echo "It's online... Proceed init"
+	fi
+done
 
 exec "${@}"
