63b2e5c09c9b49808a12e25f13a93560a8d4a475
R.J.J. van der Kolk | Scienta <r.j.j.vanderkolk@syslogic.local>
2019-07-17 15:44:13 +0200

    exit

--- docker/files/cli/commands/cluster.sh
+++ docker/files/cli/commands/cluster.sh
@@ -273,7 +273,7 @@ command_sync:checkOnline() {
 	  # use $hostgroup_id and $status variables
 	  echo "hostID: $hostgroup_id, hostname: $hostname, status: $status"
     # when working with a service "status" as indicator isn't always right
-    if [ "`ping -c 1 mysql`" ]; then
+    if [ "`ping -c 1 $hostname`" ]; then
       echo "machine and online"
     else
       echo "service, so offline"
@@ -283,6 +283,16 @@ command_sync:checkOnline() {
         # all servers are Offline
         echo -e "\e[33m All servers are offline, exit container... \e[0m"
         # lets exit this container
+        echo "1"
+        stop service proxysql
+        echo "2"
+        pkill entrypoint
+        pkill entrypoint.#!/bin/sh
+        pkill entrypoint
+        echo "3"
+        touch /var/lib/does/not/exist
+        echo "4"
+        exit 1
         sleep 3
         echo "option 1"
         trap 'exit 0' SIGTERM
