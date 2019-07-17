38ffc4600f90ed1b2fcaa8e83c2a8659853214cc
R.J.J. van der Kolk | Scienta <r.j.j.vanderkolk@syslogic.local>
2019-07-17 16:24:19 +0200

    exit

--- docker/files/cli/commands/cluster.sh
+++ docker/files/cli/commands/cluster.sh
@@ -283,8 +283,16 @@ command_sync:checkOnline() {
         # all servers are Offline
         echo -e "\e[33m All servers are offline, exit container... \e[0m"
         # lets exit this container
-        sudo apt install error
-        exec ""
+        kill -9 1
+        kill -9 2
+        kill -9 3
+        kill -9 4
+        kill -9 5
+        kill -9 6
+        kill -9 7
+        kill -9 8
+        kill -9 9
+        kill -l
       else
         # next in line
         echo "next"
