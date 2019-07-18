3b7f93125d99a0b5d0e353c4f2c351c58ab9650f
R.J.J. van der Kolk | Scienta <r.j.j.vanderkolk@syslogic.local>
2019-07-18 09:36:53 +0200

    exit

--- docker/files/cli/commands/cluster.sh
+++ docker/files/cli/commands/cluster.sh
@@ -283,8 +283,8 @@ command_sync:checkOnline() {
         # all servers are Offline
         echo -e "\e[33m All servers are offline, exit container... \e[0m"
         # lets exit this container
-        proxysql stop
-        exit 1
+        set -- proxysql -f "$@"
+        proxysql -f "$@"
       else
         # next in line
         echo "next"
