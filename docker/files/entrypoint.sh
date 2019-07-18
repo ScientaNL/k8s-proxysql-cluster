d941cf38655b8e6cba45d1be0fc72bc827f5d83f
Rub& <r.j.j.vanderkolk@syslogic.nl>
2019-07-18 14:41:51 +0200

    onlineCheck (#52)
    Added some usefull info and updated the deployment.yaml

--- docker/files/entrypoint.sh
+++ docker/files/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+touch /proxysql-liveness
+
 if [[ ! -z $CONFIG_TEMPLATE && -s "$CONFIG_TEMPLATE" ]]; then
 	envsubst < $CONFIG_TEMPLATE > /etc/proxysql.cnf
 	echo "Replaced content and variables from $CONFIG_TEMPLATE into /etc/proxysql.cnf"
